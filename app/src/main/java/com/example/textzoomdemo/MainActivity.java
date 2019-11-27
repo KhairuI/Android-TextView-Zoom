a3276e937a24e28f493e83cb8223da0c04881a55
Khariul <khairul4166@gmail.com>
2019-11-28 01:57:34 +0600

    Android TextView Zoom

---
+++ app/src/main/java/com/example/textzoomdemo/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.textzoomdemo;
+
+import android.graphics.PointF;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    float[] lastEvent = null;
+    float d = 0f;
+    float newRot = 0f;
+    private boolean isZoomAndRotate;
+    private boolean isOutSide;
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+    private int mode = NONE;
+    private PointF start = new PointF();
+    private PointF mid = new PointF();
+    float oldDist = 1f;
+    private float xCoOrdinate, yCoOrdinate;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView= findViewById(R.id.textViewId);
+
+        textView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                TextView view = (TextView) v;
+                view.bringToFront();
+                viewTransformation(view, event);
+                return true;
+            }
+        });
+    }
+
+
+    private void viewTransformation(View view, MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                xCoOrdinate = view.getX() - event.getRawX();
+                yCoOrdinate = view.getY() - event.getRawY();
+
+                start.set(event.getX(), event.getY());
+                isOutSide = false;
+                mode = DRAG;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f) {
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+
+                lastEvent = new float[4];
+                lastEvent[0] = event.getX(0);
+                lastEvent[1] = event.getX(1);
+                lastEvent[2] = event.getY(0);
+                lastEvent[3] = event.getY(1);
+                d = rotation(event);
+                break;
+            case MotionEvent.ACTION_UP:
+                isZoomAndRotate = false;
+                if (mode == DRAG) {
+                    float x = event.getX();
+                    float y = event.getY();
+                }
+            case MotionEvent.ACTION_OUTSIDE:
+                isOutSide = true;
+                mode = NONE;
+                lastEvent = null;
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!isOutSide) {
+                    if (mode == DRAG) {
+                        isZoomAndRotate = false;
+                        view.animate().x(event.getRawX() + xCoOrdinate).y(event.getRawY() + yCoOrdinate).setDuration(0).start();
+                    }
+                    if (mode == ZOOM && event.getPointerCount() == 2) {
+                        float newDist1 = spacing(event);
+                        if (newDist1 > 10f) {
+                            float scale = newDist1 / oldDist * view.getScaleX();
+                            view.setScaleX(scale);
+                            view.setScaleY(scale);
+                        }
+                        if (lastEvent != null) {
+                            newRot = rotation(event);
+                            view.setRotation((float) (view.getRotation() + (newRot - d)));
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private float rotation(MotionEvent event) {
+        double delta_x = (event.getX(0) - event.getX(1));
+        double delta_y = (event.getY(0) - event.getY(1));
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (int) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
+
+
+
+
+}
